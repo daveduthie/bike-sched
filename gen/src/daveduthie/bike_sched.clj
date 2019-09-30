05c1bbc3236fb9ff6e0a4c1cac25d04ffcee5134
David Duthie <dave.a.duthie@gmail.com>
2019-09-30 13:25:43 +0200

    Check whether I like ints as ids.

--- gen/src/daveduthie/bike_sched.clj
+++ gen/src/daveduthie/bike_sched.clj
@@ -5,7 +5,8 @@
             [expound.alpha :as expound]
             [loom.graph :as graph]
             [loom.label]
-            loom.io))
+            loom.io
+            [clojure.walk :as walk]))
 
 (set! *print-namespace-maps* false)
 (set! s/*explain-out* expound/printer)
@@ -114,61 +115,76 @@
 
 (def project-gen* (iterate add-task* project-seed*))
 
-#_(s/explain :project/schedule
-             (nth (gen/sample-seq (nth project-gen* 1)) 10))
-
-#:project{:resources {#uuid "d8f18b9f-bce2-494a-80c1-5565d041fe75" ["1xukwN6X0j" 1],
-                      #uuid "e535d21e-eabe-4ce1-b2e6-b3c5254edfcf" ["KHcDNC608" 8],
-                      #uuid "baef6179-61bb-4141-8d8c-beda76e27fc9" ["Eu2" 334],
-                      #uuid "ae343099-3a7a-4424-b456-ce6776f2a5d9" ["Ye24" 2],
-                      #uuid "11402437-b2bf-435f-927b-066d034706f7" ["70j76w" 221],
-                      #uuid "f6c9369f-2364-4e9f-a616-4087f4015354" ["ley" 0],
-                      #uuid "2342d3b5-99cb-4039-99e5-346aa8dc7e0b" ["3t6" 60],
-                      #uuid "cd8afe58-d427-4127-b658-be1ca5fd4843" ["9r1WyrffPg" 1]},
-          :tasks     {#uuid "30678e34-1d23-48a5-9cce-62312ef39f5d"
-                      #:task{:deps [],
-                             :modes
-                             {#uuid "8c2a265f-f68f-43ca-921f-af30c8cbcfdf"
-                              #:mode{:duration     380,
-                                     :requirements #{[10 #uuid "f6c9369f-2364-4e9f-a616-4087f4015354"]
-                                                     [3 #uuid "11402437-b2bf-435f-927b-066d034706f7"]
-                                                     [9 #uuid "cd8afe58-d427-4127-b658-be1ca5fd4843"]
-                                                     [8 #uuid "d8f18b9f-bce2-494a-80c1-5565d041fe75"]
-                                                     [8 #uuid "ae343099-3a7a-4424-b456-ce6776f2a5d9"]
-                                                     [7 #uuid "baef6179-61bb-4141-8d8c-beda76e27fc9"]
-                                                     [4 #uuid "e535d21e-eabe-4ce1-b2e6-b3c5254edfcf"]
-                                                     [5 #uuid "2342d3b5-99cb-4039-99e5-346aa8dc7e0b"]}},
-                              #uuid "151b9906-a43e-45bd-85e7-fc016ba20acf"
-                              #:mode{:duration     179,
-                                     :requirements #{[3 #uuid "cd8afe58-d427-4127-b658-be1ca5fd4843"]}},
-                              #uuid "c88655e6-64e9-4fad-85f1-ee315c65c49a"
-                              #:mode{:duration     472,
-                                     :requirements #{[10 #uuid "baef6179-61bb-4141-8d8c-beda76e27fc9"]
-                                                     [5 #uuid "cd8afe58-d427-4127-b658-be1ca5fd4843"]
-                                                     [3 #uuid "11402437-b2bf-435f-927b-066d034706f7"]
-                                                     [8 #uuid "d8f18b9f-bce2-494a-80c1-5565d041fe75"]
-                                                     [9 #uuid "f6c9369f-2364-4e9f-a616-4087f4015354"]
-                                                     [5 #uuid "e535d21e-eabe-4ce1-b2e6-b3c5254edfcf"]
-                                                     [4 #uuid "2342d3b5-99cb-4039-99e5-346aa8dc7e0b"]}},
-                              #uuid "39defdc8-cfb6-4684-aaf1-5433972ff3dd"
-                              #:mode{:duration     78,
-                                     :requirements #{[3 #uuid "2342d3b5-99cb-4039-99e5-346aa8dc7e0b"]
-                                                     [7 #uuid "f6c9369f-2364-4e9f-a616-4087f4015354"]
-                                                     [7 #uuid "d8f18b9f-bce2-494a-80c1-5565d041fe75"]
-                                                     [7 #uuid "baef6179-61bb-4141-8d8c-beda76e27fc9"]
-                                                     [5 #uuid "e535d21e-eabe-4ce1-b2e6-b3c5254edfcf"]}},
-                              #uuid "f7a6bd7a-4fcd-4f04-8d9b-d23f5747f991"
-                              #:mode{:duration     154,
-                                     :requirements #{[8 #uuid "f6c9369f-2364-4e9f-a616-4087f4015354"]
-                                                     [2 #uuid "baef6179-61bb-4141-8d8c-beda76e27fc9"]
-                                                     [7 #uuid "d8f18b9f-bce2-494a-80c1-5565d041fe75"]
-                                                     [4 #uuid "e535d21e-eabe-4ce1-b2e6-b3c5254edfcf"]
-                                                     [2 #uuid "2342d3b5-99cb-4039-99e5-346aa8dc7e0b"]}}}}}}
-
-
 (defn sample-project
   ([size] (gen/generate (nth project-gen* size) size))
   ([size seed] (gen/generate (nth project-gen* size) size seed)))
+
+(comment
+  ;;; Experimenting with ints as ids. Quite like how it makes the
+  ;;; schedule more compact.
+  (let [p      (sample-project 2 2)
+        ctr    (volatile! -1)
+        lookup (volatile! {})
+        ->int  (fn [id]
+                 (if-let [int-id (get @lookup id)]
+                   int-id
+                   (let [int-id (vswap! ctr inc)]
+                     (vswap! lookup assoc id int-id)
+                     int-id)))
+        p'     (-> p
+                   (update
+                    :project/resources
+                    (fn [resources]
+                      (into []
+                            (map val)
+                            (walk/postwalk (fn [x]
+                                             (if (uuid? x) (->int x) x))
+                                           resources))))
+                   (update :project/tasks
+                           (fn [tasks]
+                             (walk/postwalk
+                              (fn [x] (if-let [int-id (@lookup x)]
+                                        int-id x))
+                              tasks))))
+        p''    (-> p'
+                   (update :project/tasks
+                           (fn [tasks]
+                             (into {}
+                                   (map (fn [[task-id task]]
+                                          [task-id
+                                           (update task :task/modes
+                                                   (fn [modes]
+                                                     (into []
+                                                           (vals modes))))]))
+                                   tasks))))
+        _      (vreset! ctr -1)
+        p'''   (update p'' :project/tasks
+                       (fn [tasks]
+                         (into []
+                               (map val)
+                               (walk/postwalk (fn [x]
+                                                (if (uuid? x)
+                                                  (->int x)
+                                                  x))
+                                              tasks))))]
+    p''')
+
+  {:project/resources [["Ni" 1] ["A" 2] ["0" 1] ["yD" 0] ["i" 0] ["2" 1]],
+   :project/tasks
+   [{:task/deps  [],
+     :task/modes [{:mode/duration     17,
+                   :mode/requirements [{:req/id 3, :req/quant 10}]}
+                  {:mode/duration     577,
+                   :mode/requirements [{:req/id 3, :req/quant 5}]}
+                  {:mode/duration     641,
+                   :mode/requirements [{:req/id 1, :req/quant 6}]}]}
+    {:task/deps  [0],
+     :task/modes [{:mode/duration     647,
+                   :mode/requirements [{:req/id 4, :req/quant 6}]}
+                  {:mode/duration     3,
+                   :mode/requirements [{:req/id 2, :req/quant 10}
+                                       {:req/id 0, :req/quant 8}
+                                       {:req/id 5, :req/quant 3}]}]}]})
 
 (defn post-schedule! [schedule]
   (:body
